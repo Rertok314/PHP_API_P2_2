9b5754a687d724682c7ee1360499fca03270b737
Rertok314 <brasil.elias@hotmail.com>
2026-05-28 13:11:26 -0300

    ajuste de rota

--- routes/web.php
+++ routes/web.php
@@ -4,5 +4,6 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ProductController;
 
 Route::post('/products', [ProductController::class, 'store']);
+Route::get('/products', [ProductController::class, 'index']);
 Route::get('/', [ProductController::class, 'index']);
 Route::get('/api-view', [ProductController::class, 'apiView']);
